48166119b5a0daecdf02882ac97188cafd11bd9d
Xingzhong <xingzhong@me.com>
2023-05-02 14:12:54 +0800

    update

---
+++ pelicanconf.py
@@ -0,0 +1,29 @@
+AUTHOR = '小九'
+SITENAME = '朝花夕拾'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'Europe/Rome'
+
+DEFAULT_LANG = 'cn'
+
+# THEME = "/Users/xingzhong/pelican-themes/bootstrap"
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = None
+
+# Social widget
+SOCIAL = None
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
